638f5114302bb8a1319278ba1f5dfa3f29ec9e60
SAHIL TAGRA <sahiltagra3554@gmail.com>
2021-09-06 18:23:39 +0530

    first commit

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./ErrorPage.css";
-const ErrorPage = () => {
+const ErrorPage = (props) => {
   return (
     <div className="error-page">
       <h1 className="error-code">404</h1>
       <p className="error-disc">Page You Are Looking for Doesnot Exist</p>
+      <button
+        className="btn btn-danger"
+        onClick={() => props.history.push("/login")}
+      >
+        Go Back
+      </button>
     </div>
   );
 };
